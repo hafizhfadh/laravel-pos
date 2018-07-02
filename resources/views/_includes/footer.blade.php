1ce067a1a932fec55769c4f87ed6940d803002b3
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2018-07-02 11:35:20 +0700

    Initial Commit

---
+++ resources/views/_includes/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-sm-none d-md-block">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
